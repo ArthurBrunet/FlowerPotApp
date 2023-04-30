6443da56da2ab824f552200f0d5be2fd608f6153
Arthur Brunet <Arthur.brunet76@gmail.com>
2023-04-30 18:54:52 +0200

    splash Screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './src/router/Router';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import moment from 'moment';
 import 'moment/locale/fr';
+import SplashScreen from 'react-native-splash-screen';
 moment.locale('fr');
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <SafeAreaProvider>
       <Router />
